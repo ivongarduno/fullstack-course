0adea8386bc9b90aa65ec292d6b5e704b3d95284
ivongarduno <ivonne_1412@hotmail.com>
2021-01-14 11:02:25 -0600

    backend con heroku

--- part2/notes/src/services/notes.js
+++ part2/notes/src/services/notes.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/api/notes";
+const baseUrl = "https://fullstack-notes-2.herokuapp.com/api/notes";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
